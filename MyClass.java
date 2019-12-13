7243dc71086b623616ab2ce71b1c0b11bfc2d27c
kami.manzoor@gmail.com <kami.manzoor@gmail.com>
2019-12-13 14:16:01 +0000

    Modify

--- MyClass.java
+++ MyClass.java
@@ -1,10 +1,26 @@
 public class MyClass.java {
-  public static void main(String[]args){
-  int j = 0;
-  for (int i=0;i<10;j++) {
-  i++;
-  System.out.println("HEllo");
+  public static void main(String[] args) {
+  try {
+    doSomethingWhichThrowsException();
+    System.out.println("OK");   // incorrect "OK" message is printed
+  } catch (RuntimeException e) {
+    System.out.println("ERROR");  // this message is not shown
   }
-  
+}
+
+public static void doSomethingWhichThrowsException() {
+  try {
+    throw new RuntimeException();
+  } finally {
+    for (int i = 0; i < 10; i ++) {
+      //...
+      if (q == i) {
+        break; // ignored
+      }
+    }
+
+    /* ... */
+    return;      // Noncompliant - prevents the RuntimeException from being propagated
   }
+}
 }
